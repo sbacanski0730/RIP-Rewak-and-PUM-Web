759aaa16c5843460547e1f01742a6d4518b4219f
Szymon Bacanski <s.bacanski@gmail.com>
2022-12-02 10:46:56 +0100

    navbar styled added

--- src/components/Navbar/Navbar.style.js
+++ src/components/Navbar/Navbar.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledNavbar = styled.div`
-	border: 1px solid white;
+	/* border: 1px solid white; */
 
 	position: absolute;
 	top: 0;
@@ -10,4 +10,36 @@ export const StyledNavbar = styled.div`
 	display: flex;
 	flex-direction: row;
 	gap: 20px;
+	padding: 10px 10px;
+
+	.link-wrapper {
+		padding: 10px 10px;
+
+		a {
+			text-decoration: none;
+			color: #fff;
+			position: relative;
+		}
+		a::after {
+			content: '';
+			position: absolute;
+			width: 100%;
+			transform: scaleX(0);
+			height: 1px;
+			bottom: 0;
+			left: 0;
+			background-color: #fff;
+			transform-origin: bottom left;
+			transition: transform 0.09s ease-out;
+		}
+
+		a:hover {
+			cursor: pointer;
+		}
+
+		a:hover::after {
+			transform: scaleX(1);
+			transform-origin: bottom left;
+		}
+	}
 `;
