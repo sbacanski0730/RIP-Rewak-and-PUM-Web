f5433344abf69362a3bed528af1db3006b4e9455
Szymon Bacanski <s.bacanski@gmail.com>
2022-12-11 21:31:45 +0100

    adding filter to workers search page

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { StyledInput } from './Input.style';
 
-const Input = ({ placeholder }) => {
+const Input = ({ placeholder, filterList }) => {
 	const [inputValue, setInputValue] = useState();
 	return (
 		<>
 			<StyledInput>
-				<input type='text' placeholder={placeholder} value={inputValue} />
+				<input
+					type='text'
+					placeholder={placeholder}
+					value={inputValue}
+					onChange={filterList}
+				/>
 			</StyledInput>
 		</>
 	);
